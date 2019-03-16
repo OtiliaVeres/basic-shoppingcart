fc9fdc9b942181f1d27d20acf6d2cc45e8f0e393
Otilia Veres <otilia.r.veres@gmail.com>
2019-03-16 21:10:49 +0100

    Added service add and list products

--- src/main/java/basicshoppingcart/model/Product.java
+++ src/main/java/basicshoppingcart/model/Product.java
@@ -1,4 +1,4 @@
-package model;
+package basicshoppingcart.model;
 
 public class Product {
 
---
+++ src/main/java/basicshoppingcart/service/basicshoppingcartService.java
@@ -0,0 +1,21 @@
+package basicshoppingcart.service;
+
+import java.util.List;
+
+import basicshoppingcart.model.Product;
+
+public class basicshoppingcartService {
+
+	public static Product addProduct(Product pProdcut) {
+
+		return pProdcut;
+		
+	}
+
+	public static List<Product> listProducts(List<Product> list) {
+		
+		return list;
+		
+	}
+
+}
