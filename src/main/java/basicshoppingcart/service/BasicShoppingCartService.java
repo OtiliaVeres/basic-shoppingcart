827e939d541ac4961509352a8a737f987c74f448
Otilia Veres <otilia.r.veres@gmail.com>
2019-03-17 20:52:47 +0100

    Refactor tipe functions of class Service

--- src/main/java/basicshoppingcart/service/BasicShoppingCartService.java
+++ src/main/java/basicshoppingcart/service/BasicShoppingCartService.java
@@ -6,13 +6,13 @@ import basicshoppingcart.model.Product;
 
 public class BasicShoppingCartService {
 
-	public static Product addProduct(Product pProdcut) {
+	public Product addProduct(Product pProdcut) {
 
 		return pProdcut;
 		
 	}
 
-	public static List<Product> listProducts(List<Product> list) {
+	public List<Product> listProducts(List<Product> list) {
 		
 		return list;
 		
